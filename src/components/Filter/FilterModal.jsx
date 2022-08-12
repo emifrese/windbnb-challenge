fee16bdb80048bf3ca1eaf9ad97f2ef07d8d4f8a
unknown <emilianomartindominguezfrese@gmail.com>
2022-08-12 19:58:55 -0300

    finished desktop styles / remain modal in mobile view

--- src/components/Filter/FilterModal.jsx
+++ src/components/Filter/FilterModal.jsx
@@ -3,54 +3,78 @@ import React, { useState } from "react";
 import classes from "./FilterModal.module.css";
 
 import searchImg from "../../assets/search_FILL0_wght400_GRAD0_opsz48.svg";
+import { useEffect } from "react";
 
 const FilterModal = () => {
   const [focusInput, setFocusInput] = useState("Location");
 
-  return (
-    <div className={classes.filterContainer}>
-      <div
-        className={classes.locationContainer}
-        style={
-          focusInput === "Location" ? {
-            borderWidth: "1px",
-            borderColor: "#333333",
-            borderStyle: "solid",
-            borderRadius: "16px",
-          } : {}
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth <= 375) {
+      setIsMobile((prevState) => {
+        if(!prevState) {
+          return true
+        } else {
+          return prevState
         }
-        onClick={() =>
-          setFocusInput("Location")
+      });
+    } else {
+      setIsMobile((prevState) => {
+        if (prevState) {
+          return false;
+        } else {
+          return prevState
         }
-      >
-        <h3>LOCATION</h3>
-        <input className={classes.locationInput} value={"Helsinki, Finland"} />
-      </div>
-      <div
-        className={classes.guestContainer}
-        style={
-          focusInput === "Guest" ? {
-            borderWidth: "1px",
-            borderColor: "#333333",
-            borderStyle: "solid",
-            borderRadius: "16px",
-          } : {}
-        }
-        onClick={() =>
-          setFocusInput("Guest")
-        }
-      >
-        <h3>GUEST</h3>
-        <input className={classes.guestInput} placeholder={"Add guests"} />
-      </div>
-      <div className={classes.searchContainer}>
-        <button className={classes.searchButton}>
-          <img className={classes.searchImg} src={searchImg} alt="search" />
-          <p>Search</p>
-        </button>
-      </div>
+      });
+    }
+  }, []);
+
+
+  const content = isMobile ? (<p>Mobile</p>) : (<div className={classes.filterContainer}>
+    <div
+      className={classes.locationContainer}
+      style={
+        focusInput === "Location"
+          ? {
+              borderWidth: "1px",
+              borderColor: "#333333",
+              borderStyle: "solid",
+              borderRadius: "16px",
+            }
+          : {}
+      }
+      onClick={() => setFocusInput("Location")}
+    >
+      <h3>LOCATION</h3>
+      <input className={classes.locationInput} value={"Helsinki, Finland"} />
     </div>
-  );
+    <div
+      className={classes.guestContainer}
+      style={
+        focusInput === "Guest"
+          ? {
+              borderWidth: "1px",
+              borderColor: "#333333",
+              borderStyle: "solid",
+              borderRadius: "16px",
+            }
+          : {}
+      }
+      onClick={() => setFocusInput("Guest")}
+    >
+      <h3>GUEST</h3>
+      <input className={classes.guestInput} placeholder={"Add guests"} />
+    </div>
+    <div className={classes.searchContainer}>
+      <button className={classes.searchButton}>
+        <img className={classes.searchImg} src={searchImg} alt="search" />
+        <p>Search</p>
+      </button>
+    </div>
+  </div>)
+
+  return content;
 };
 
 export default FilterModal;
--- src/components/Guest/GuestToggle.jsx
+++ src/components/Guest/GuestToggle.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
-const GuestToggle = ({type}) => {
+import classes from "./GuestToggle.module.css";
 
-    const [quantity, setQuantity] = useState(0);
+const GuestToggle = ({ type }) => {
+  const [quantity, setQuantity] = useState(0);
 
   return (
-    <div>
-        <h3>{type}</h3>
-        <p>Agues 13 or above</p>
-        <button>-</button>
-        <input value={quantity}/>
-        <button>+</button>
+    <div className={classes.guestToggle}>
+      <h3>{type}</h3>
+      <p>{type === "Adult" ? "Ages 13 or above" : "Ages 2-12"}</p>
+      <div className={classes.inputContainer}>
+        <button
+          onClick={() =>
+            setQuantity((prevState) => {
+              if (prevState > 0) {
+                return prevState - 1;
+              } else {
+                return prevState;
+              }
+            })
+          }
+        >
+          -
+        </button>
+        <input value={quantity} />
+        <button
+          onClick={() =>
+            setQuantity((prevState) => prevState + 1)
+          }
+        >
+          +
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GuestToggle
+export default GuestToggle;
